d1628bc7ef8d58bb4def668eeb1d0610bcb3790d
Hari Pandian <haripandian0212@gmail.com>
2026-06-06 09:01:54 +0530

    Modified Files

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "safetapintern.vercel.app",
+    "https://safetapintern.vercel.app",
   ],
 }));
 app.use(express.json());
